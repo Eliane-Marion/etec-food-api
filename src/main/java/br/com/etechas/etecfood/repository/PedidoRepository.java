669cfa6e90e2e41719dff03eb0b91ada70ef4c8f
viccitor <victor.m.marley@gmail.com>
2025-11-10 12:40:01 -0300

    Adicionei o nome dos integrantes do grupo nas classes, interfaces e enum do grupo

--- src/main/java/br/com/etechas/etecfood/repository/PedidoRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/PedidoRepository.java
@@ -1,4 +1,14 @@
-package br.com.etechas.etecfood.repository;
+/*
+ * Nomes:
+ * Victor de Melo - Github: Viccitor
+ * Miguel Angel - Github: miguel320007
+ * Anderson Filipim - Github: AndersonFilipimdeSousaFilho
+ * Gabriel de Albuquerque - Github: GabrieldeAlbuquerqueMagalhaes
+ * Jonatas Calebe - Github: calebe-007
+ * Cauã Santos - Github: CauaSantos41
+ * */
+
+ br.com.etechas.etecfood.repository;
 
 import br.com.etechas.etecfood.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
