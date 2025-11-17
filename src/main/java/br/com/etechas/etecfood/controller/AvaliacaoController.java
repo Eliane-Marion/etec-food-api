29839bda601d6523e85f0e8ab5556db7e70c2bab
viccitor <victor.m.marley@gmail.com>
2025-11-17 13:52:18 -0300

    Adcionei as anotacoes RestController e ResquestMapping na classe AvaliacaoController

--- src/main/java/br/com/etechas/etecfood/controller/AvaliacaoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/AvaliacaoController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RestController
+@RequestMapping("/avaliacoes")
 public class AvaliacaoController {
+
     @Autowired
     private AvaliacaoRepository avaliacaoRepository;
 
