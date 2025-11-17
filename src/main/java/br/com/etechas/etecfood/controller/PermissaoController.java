3bb454437f4f64e09c368c00428928d733f5e70d
Roger Gabriel Novais WOlf <rogergnovaiswolf@gmail.com>
2025-11-17 14:41:13 -0300

    PermissaoController atualizado

--- src/main/java/br/com/etechas/etecfood/controller/PermissaoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/PermissaoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/permissao")
 // Classe responsável por gerenciar as requisições relacionadas aos usuários.
 public class PermissaoController {
 
